f082f649ed4c60cc26eeea1c013a49e93fdbfdb3
Jeremiah England <englandtuning@gmail.com>
2022-09-29 08:14:03 -0400

    style: Reformat with Black

--- fra_rss/middlewares.py
+++ fra_rss/middlewares.py
@@ -53,7 +53,7 @@ class FraRssSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class FraRssDownloaderMiddleware:
@@ -100,4 +100,4 @@ class FraRssDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
--- fra_rss/spiders/newsroom.py
+++ fra_rss/spiders/newsroom.py
@@ -4,13 +4,15 @@ import json
 
 
 class NewsroomSpider(scrapy.Spider):
-    name = 'newsroom'
-    allowed_domains = ['railroads.dot.gov']
-    start_urls = ['https://railroads.dot.gov/newsroom/press-releases']
+    name = "newsroom"
+    allowed_domains = ["railroads.dot.gov"]
+    start_urls = ["https://railroads.dot.gov/newsroom/press-releases"]
 
     def parse(self, response: Response):
-        selectors: scrapy.SelectorList = response.css('.title--news-item').css('a')
+        selectors: scrapy.SelectorList = response.css(".title--news-item").css("a")
         for selector in selectors:
             title = selector.css("::text").get()
             path = selector.attrib["href"]
-            print(json.dumps({"title": title, "link": f"https://railroads.dot.gov{path}"}))
+            print(
+                json.dumps({"title": title, "link": f"https://railroads.dot.gov{path}"})
+            )
